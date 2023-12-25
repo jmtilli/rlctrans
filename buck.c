2147a2d11044c3817f469b5a93c88c8291652bf3
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2023-12-25 11:49:42 +0200

    Remove non-needed includes

--- buck.c
+++ buck.c
@@ -1,8 +1,4 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <lapack.h>
-#include <math.h>
 #include "libsimul.h"
 
 const double dt = 1e-7; // 100 ns
--- rectifier.c
+++ rectifier.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <lapack.h>
 #include <math.h>
 #include "libsimul.h"
 
